222401451d279f558d66d3968f33f3fa0f2855e2
ebdem <ebu.bekir.3333@gmail.com>
2020-10-18 16:47:34 +0300

    added actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
